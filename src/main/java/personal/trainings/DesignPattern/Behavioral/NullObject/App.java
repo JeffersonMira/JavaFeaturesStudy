e782ca6512b986e8659b47001a92e7feb910a6eb
Da Silva Mira <jfmr@gft.com>
2019-07-30 10:47:48 -0300

    nullObject Pattern

---
+++ src/main/java/personal/trainings/DesignPattern/Behavioral/NullObject/App.java
@@ -0,0 +1,15 @@
+package personal.trainings.DesignPattern.Behavioral.NullObject;
+
+/**
+ * You create an objectFactor to return the object. This object has an abstractClass
+ * that it extends from. Then the factor checks if the object exists.. if it exists
+ * it returns an implementation of this abstractClass with real data.
+ * If there is no object to be returned, it returns an implementation of abstractClass 
+ * that is the nullObject - an object just to inform that there is no available data. 
+ * 
+ * Not sure if it worth the effort. I dont think so. 
+ *
+ */
+public class App {
+	public static void main(String[] args) { }
+}
