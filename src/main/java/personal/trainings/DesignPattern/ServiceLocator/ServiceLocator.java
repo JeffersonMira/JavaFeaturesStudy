022abc1e28cc6f718b0b925e0bae630d31af086e
Da Silva Mira <jfmr@gft.com>
2019-08-05 10:51:07 -0300

    Service Locator and JNDI patterns

---
+++ src/main/java/personal/trainings/DesignPattern/ServiceLocator/ServiceLocator.java
@@ -0,0 +1,20 @@
+package personal.trainings.DesignPattern.ServiceLocator;
+
+public class ServiceLocator {
+
+	private static Cache cache = new Cache();
+	
+	public static Service getService(String jndiName) {
+		
+		Service service = cache.getService(jndiName);
+		
+		if( service != null ) return service;
+		
+		InitialContext context = new InitialContext();
+		Service s = (Service) context.lookup(jndiName);
+		cache.addService(s);
+		
+		return s;
+	}
+	
+}
